ad561a1b0f5709e132c3fbc561578faaeb4bb857
malvina <malvina.deljunco@iesvelazquez.org>
2019-01-21 13:30:42 +0100

    1 componente creado

--- helloWorld/src/app/app.module.ts
+++ helloWorld/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimerComponenteComponent
   ],
   imports: [
     BrowserModule
