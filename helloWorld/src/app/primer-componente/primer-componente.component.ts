0165aae09918b377032524247d7b2cd9527289c1
malvina <malvina.deljunco@iesvelazquez.org>
2019-01-21 13:45:32 +0100

    Primer componente funcionando

--- helloWorld/src/app/primer-componente/primer-componente.component.ts
+++ helloWorld/src/app/primer-componente/primer-componente.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
+@Component({ // definimos componente
   selector: 'app-primer-componente',
   templateUrl: './primer-componente.component.html',
   styleUrls: ['./primer-componente.component.css']
