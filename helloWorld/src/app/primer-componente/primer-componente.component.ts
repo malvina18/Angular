4a1f04f4efa8979316d8e316331d7656379b18ec
malvina <malvina.deljunco@iesvelazquez.org>
2019-01-21 14:32:40 +0100

    Ejemplos array

--- helloWorld/src/app/primer-componente/primer-componente.component.ts
+++ helloWorld/src/app/primer-componente/primer-componente.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({ // definimos componente
   selector: 'app-primer-componente',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimerComponenteComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  miVariable = 'Esta es mi variable de js en angular';
+  miArray: string [];
+  /* tipado :miArray: string ['Pan', 'Casa', 'Camino', 'Destino'];*/
 
   ngOnInit() {
+    this.miArray = ['Pan', 'Casa', 'Camino', 'Destino'];
   }
 
 }
