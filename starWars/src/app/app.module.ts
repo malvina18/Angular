03986f8fa4d575608f0596f00f3e4aa38125020e
malvina <malvina.deljunco@iesvelazquez.org>
2019-01-30 13:38:16 +0100

    starWarsBotones

--- starWars/src/app/app.module.ts
+++ starWars/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule , LOCALE_ID} from '@angular/core';
+
+import {registerLocaleData} from '@angular/common';
+import localEs from '@angular/common/locales/es';
 
 import { HttpClientModule } from '@angular/common/http';
+// El segundo parametro  'es' para configurar angula en español
+registerLocaleData(localEs, 'es');
 
 import { AppComponent } from './app.component';
 import { PersonajesComponent } from './personajes/personajes.component';
@@ -15,9 +20,9 @@ import { PlanetaComponent } from './planeta/planeta.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'es'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- starWars/src/app/personajes/personajes.component.ts
+++ starWars/src/app/personajes/personajes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {PAjaxService } from '../p-ajax.service';
+import {Component, OnInit} from '@angular/core';
+import {PAjaxService} from '../p-ajax.service';
 
 @Component({
   selector: 'app-personajes',
@@ -10,15 +10,20 @@ export class PersonajesComponent implements OnInit {
 
   public datos: any; // todo lo que me va  a responder la api
   public listaPer: Object; // es lista de personas (lo que yo quiero pintar).
-  public datosPlaneta:  Object = null;
+  public datosPlaneta: Object = null;
 
   constructor(private serviciopAjax: PAjaxService) {
     // Hacemos la petición Ajax:
     serviciopAjax.peti().subscribe(daticos => {
       console.log(daticos);
-      this.datos = daticos;
-      this.listaPer = this.datos.results;
+     this.asignarDatos(daticos);
     });
+  }
+
+  private asignarDatos(datos: Object) {
+    this.datos = datos;
+    console.log(this.datos);
+    this.listaPer = this.datos.results;
   }
 
   ngOnInit() {
@@ -28,14 +33,31 @@ export class PersonajesComponent implements OnInit {
   // callback  ESta funcion se ejecuta cuando click en el enlace.
   pedirPlaneta(evento, url: string) {
     evento.preventDefault();
-    console.log(url + ' ?format=json');
+    console.log(url + '?format=json');
     this.serviciopAjax.petiAurl(url + '?format=json').subscribe(res => {
+      console.log('pido planeta');
       console.log(res);
       // se ejecuta cuando click en el enlace.
       this.datosPlaneta = res;
     });
   }
 
+// Hacemos una peticion ajax en la que contiene la url 'next'
+  siguientePer() {
+    this.serviciopAjax.petiAurl(this.datos.next).subscribe(daticos => {
+      this.datos = daticos;
+      this.asignarDatos(daticos);
+      // this.listaPer = this.datos.results;
+    });
+  }
 
+// Hacemos una peticion ajax en la que contiene la url 'previous'
+  anterioresPer() {
+    this.serviciopAjax.petiAurl(this.datos.previous).subscribe(daticos => {
+      this.datos = daticos;
+      this.asignarDatos(daticos);
+      // this.listaPer = this.datos.results;
+    });
+  }
 
 }
