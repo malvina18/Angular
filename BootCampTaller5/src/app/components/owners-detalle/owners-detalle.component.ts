2d0b6cce627cfd7f51018bed19b2fb206b067d45
malvina <malvina.deljunco@iesvelazquez.org>
2019-02-20 14:35:31 +0100

    taller5ModificarTerminado

--- BootCampTaller5/src/app/components/owners-detalle/owners-detalle.component.ts
+++ BootCampTaller5/src/app/components/owners-detalle/owners-detalle.component.ts
@@ -11,7 +11,7 @@ import {OwnersService} from '../../services/owners.service';
 export class OwnersDetalleComponent implements OnInit {
   owner: Owner;
 // ponemos el servivicio y el route / ActivatedRoute:nos permite obtener el parametro
-  constructor(private route: ActivatedRoute, private ownerService: OwnersService) {
+  constructor(private route: ActivatedRoute, private ownerService: OwnersService , private ruta: Router) {
     // creamos un objeto de tipo owner vacio.
   this.owner = <Owner>{};
 }
@@ -22,5 +22,29 @@ export class OwnersDetalleComponent implements OnInit {
     this.ownerService.getOwnerPorId(ownerId).subscribe(daticos =>
       this.owner = daticos);
     console.log(this.owner);
+  }
+
+  del(owner: Owner) {
+    console.log(owner);
+    // alert('aqui borramos');
+    const msg = '¿Estas seguro nque quieres borrar a :?' + owner.firstName + ' ' + owner.lastName;
+    if (confirm(msg)) {
+      this.ownerService.delOwnerList(owner.id).subscribe(datos => {
+        this.owner = datos;
+        console.log(this.owner);
+        this.ruta.navigate(['/owners']);
+      });
+    }
+  }
+// modificamos
+  upd(ownerId) {
+    console.log('owner id' + ownerId);
+    // alert('aqui modificamos');
+    // navegando con parametro
+    this.ruta.navigate(['/owners-add', ownerId]);
+    this.ownerService.updOwner(ownerId).subscribe(datos => {
+      this.owner = datos;
+      console.log(this.owner);
+    });
   }
 }
